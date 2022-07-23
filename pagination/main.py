6b9a2d0415011b680ed8e0b1c54125048b9bb4d0
wdse <21ypompei@wakatipu.school.nz>
2022-07-24 02:22:28 +1200

    Updates people

--- pagination/main.py
+++ pagination/main.py
@@ -1,7 +1,7 @@
-contents = ['''            <p class="text-center">print(<span style="color: orange;">'Hello World'</span>) <span style="color: red;">!=</span> print(<span style="color: orange;">'Hello'<span style="color: black;">+</span>'World'</span>)</p>
+contents = ['''            <p class="text-center paginationContent1stPageText">print(<span style="color: orange;">'Hello World'</span>) <span style="color: red;">!=</span> print(<span style="color: orange;">'Hello'<span style="color: black;">+</span>'World'</span>)</p>
             <img class="imgPag" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Earth_Western_Hemisphere_transparent_background.png/1200px-Earth_Western_Hemisphere_transparent_background.png"></img>''','''
-Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet delectus dolor odit voluptatibus, labore temporibus eveniet officia. Quas, labore ipsam. Repudiandae expedita obcaecati, eveniet officia enim qui perferendis. Dolores, consectetur.
-''','''<span style="font-size:200%;">L</span>''']
+<span class="paginationContent2ndPageText">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet delectus dolor odit voluptatibus, labore temporibus eveniet officia. Quas, labore ipsam. Repudiandae expedita obcaecati, eveniet officia enim qui perferendis. Dolores, consectetur.
+</span>''','''<span style="font-size:200%;">L</span>''']
 currentPage = 0
 
 def changePage(pgNmber):
