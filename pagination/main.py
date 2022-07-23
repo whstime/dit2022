d42d4d7fc24f21acde5d323468a1fef510b8e522
wdse <21ypompei@wakatipu.school.nz>
2022-07-24 01:39:26 +1200

    Update! (fixed bug)!

--- pagination/main.py
+++ pagination/main.py
@@ -1,6 +1,7 @@
 contents = ['''            <p class="text-center">print(<span style="color: orange;">'Hello World'</span>) <span style="color: red;">!=</span> print(<span style="color: orange;">'Hello'<span style="color: black;">+</span>'World'</span>)</p>
-            <img class="imgPag" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Earth_Western_Hemisphere_transparent_background.png/1200px-Earth_Western_Hemisphere_transparent_background.png"></img>
-''','''Hello!''']
+            <img class="imgPag" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Earth_Western_Hemisphere_transparent_background.png/1200px-Earth_Western_Hemisphere_transparent_background.png"></img>''','''
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet delectus dolor odit voluptatibus, labore temporibus eveniet officia. Quas, labore ipsam. Repudiandae expedita obcaecati, eveniet officia enim qui perferendis. Dolores, consectetur.
+''','''<span style="font-size:200%;">L</span>''']
 currentPage = 0
 
 def changePage(pgNmber):
@@ -13,33 +14,22 @@ def nextPage(*ags, **kws):
     currentPage = currentPage + 1
     changePage(currentPage)
     console.log('DEBUG - CURRENT USER PAGE = ' + str(currentPage))
-    if contents[currentPage] == contents[-1]:
-        Element('fowardBtn').element.style.visibility = 'Hidden'
-        Element('backBtn').element.style.visibility = ''
-    elif contents[currentPage] != contents[-1]:
-        Element('fowardBtn').element.style.visibility = ''
-        Element('backBtn').element.style.visibility = 'Hidden'
-    elif contents[currentPage] == contents[0]:
-        Element('backBtn').element.style.visibility = 'Hidden'
-        Element('fowardBtn').element.style.visibility = ''
-    elif contents[currentPage] != contents[0]:
-        Element('backBtn').element.style.visibility = ''
-        Element('fowardBtn').element.style.visibility = 'Hidden'
+    updateBtns()
 
 def backPage(*ags, **kws):
     global currentPage
     currentPage = currentPage - 1
     changePage(currentPage)
     console.log('DEBUG - CURRENT USER PAGE = ' + str(currentPage))
+    updateBtns()
+
+def updateBtns():
     if contents[currentPage] == contents[-1]:
         Element('fowardBtn').element.style.visibility = 'Hidden'
         Element('backBtn').element.style.visibility = ''
-    elif contents[currentPage] != contents[-1]:
-        Element('fowardBtn').element.style.visibility = ''
-        Element('backBtn').element.style.visibility = 'Hidden'
     elif contents[currentPage] == contents[0]:
         Element('backBtn').element.style.visibility = 'Hidden'
         Element('fowardBtn').element.style.visibility = ''
-    elif contents[currentPage] != contents[0]:
+    else:
         Element('backBtn').element.style.visibility = ''
-        Element('fowardBtn').element.style.visibility = 'Hidden'
+        Element('fowardBtn').element.style.visibility = ''
